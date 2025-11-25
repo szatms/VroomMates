0f15104c1080df2029db7346b664b8cdf30c80d4
Szabo Tamas <szabo.tamas.20040922@gmail.com>
2025-11-25 21:25:55 +0100

    Initialized project.

---
+++ src/main/java/com/vroommates/VroomMates/VroomMatesApplication.java
@@ -0,0 +1,13 @@
+package com.vroommates.VroomMates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomMatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VroomMatesApplication.class, args);
+	}
+
+}
