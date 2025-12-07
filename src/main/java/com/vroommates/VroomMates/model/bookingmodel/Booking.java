ff20e814bfe85148c4356fbbc3c9bdbcba8ae2b6
Szabo Tamas <szabo.tamas.20040922@gmail.com>
2025-12-07 23:39:48 +0100

    Solved ID generation mishap. Generate new DB for further testing from now on. Also removed some unnecessary comments.

--- src/main/java/com/vroommates/VroomMates/model/bookingmodel/Booking.java
+++ src/main/java/com/vroommates/VroomMates/model/bookingmodel/Booking.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 public class Booking {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookingID;
 
     @ManyToOne
